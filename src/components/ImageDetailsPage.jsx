6e04566887163718d5e177b3766040b0acc7a9b8
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-10-17 22:56:10 -0400

    Create ImageDetailsPage

---
+++ src/components/ImageDetailsPage.jsx
@@ -0,0 +1,13 @@
+
+export default function ImageDetailsPage({ artwork }) {
+    const imageUrl = `https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`;
+
+    return (
+      <div>
+        <img alt={artwork.thumbnail ? artwork.thumbnail.alt_text : artwork.title} src={imageUrl} />
+        <p>{artwork.artist_title ? artwork.artist_title : 'Unknown'}</p>
+        <p>{artwork.title}</p>
+        <p>{artwork.date_display}</p>
+      </div>
+    );
+}
